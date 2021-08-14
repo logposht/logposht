944a34086da8b212c2a5cadc216636a8a072b773
Alyreza <fanaticboy666@gmail.com>
2021-08-14 10:42:09 +0430

    add workflows

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,63 +3,63 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-	title: "لاگ‌پشت",
-	tagline: "اپلیکیشنی برای مدیریت شخصی مالی",
-	url: "https://your-docusaurus-test-site.com",
-	baseUrl: "/",
-	trailingSlash: false,
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
-	organizationName: "facebook", // Usually your GitHub org/user name.
-	projectName: "docusaurus", // Usually your repo name.
-	i18n: { defaultLocale: "fa", locales: ["fa"] },
-	themeConfig: {
-		colorMode: {
-			// "light" | "dark"
-			defaultMode: "light",
-			// Hides the switch in the navbar
-			// Useful if you want to support a single color mode
-			disableSwitch: true,
-			respectPrefersColorScheme: true,
-		},
-		navbar: {
-			title: "لاگ‌پشت",
-			logo: {
-				alt: "My Site Logo",
-				src: "img/logo.svg",
-			},
-			items: [{ to: "/blog", label: "وبلاگ", position: "left" }],
-		},
-		footer: {
-			style: "dark",
-			copyright: `کپی‌رایت این سایت مطلق است به سایت لاگ‌پشت ، سال ‌${new Date().getFullYear()}`,
-		},
-		prism: {
-			theme: lightCodeTheme,
-			darkTheme: darkCodeTheme,
-		},
-	},
-	presets: [
-		[
-			"@docusaurus/preset-classic",
-			{
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/blog/",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			},
-		],
-	],
+  title: "لاگ‌پشت",
+  tagline: "اپلیکیشنی برای مدیریت شخصی مالی",
+  trailingSlash: false,
+  url: "https://logposht.github.io", // Your website URL
+  baseUrl: "/",
+  projectName: "logposht.github.io",
+  organizationName: "logposht",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  i18n: { defaultLocale: "fa", locales: ["fa"] },
+  themeConfig: {
+    colorMode: {
+      // "light" | "dark"
+      defaultMode: "light",
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: true,
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "لاگ‌پشت",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [{ to: "/blog", label: "وبلاگ", position: "left" }],
+    },
+    footer: {
+      style: "dark",
+      copyright: `کپی‌رایت این سایت مطلق است به سایت لاگ‌پشت ، سال ‌${new Date().getFullYear()}`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,20 @@ import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 
 export default function Home() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<div className="container">
-			<div className="text--center padding-horiz--md">
-				<p> سلام خوبی ؟ </p>
-			</div>
-			<main>
-				<HomepageFeatures />
-			</main>
-			<div className={styles.buttons}>
-				<Link className="button button--secondary button--lg" to="/subscribe">
-					می خوای زودتر از بقیه امتحانش کنی ؟
-				</Link>
-			</div>
-		</div>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <div className="container">
+      <div className="text--center padding-horiz--md">
+        <p> سلام خوبی ؟ </p>
+      </div>
+      <main>
+        <HomepageFeatures />
+      </main>
+      <div className={styles.buttons}>
+        <Link className="button button--secondary button--lg" to="/subscribe">
+          می خوای زودتر از بقیه امتحانش کنی ؟
+        </Link>
+      </div>
+    </div>
+  );
 }
